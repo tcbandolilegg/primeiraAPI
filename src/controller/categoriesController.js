b213dcb9fe12b707686b49ef22fa91670dd29894
Thereza Bandoli <tcbandolilegg@gmail.com>
2021-03-27 12:23:03 -0300

    Acertar Categories Controller

--- src/controller/categoriesController.js
+++ src/controller/categoriesController.js
@@ -1,75 +1,75 @@
-const recipes = require('../modal/dataRecipes.json');
+const categories = require('../modal/dataCategories.json');
 // const express = require('express');
 const fs = require('fs');
 
 
-const allRecipes = (_, response) => {
-  response.send(recipes);
+const allCategories = (_, response) => {
+  response.send(categories);
 };
 
 
 // request é o que vem do HTTP pra o servidor como uma pergunta através da rota
-const searchRecipeForId = (request, response) => {
-  const recipeEncontrado = recipes.filter((recipe) => {
-    return recipe.id === Number(request.params.id)
+const searchCategoryForId = (request, response) => {
+  const categoryEncontrado = categories.filter((category) => {
+    return category.id === Number(request.params.id)
   })
-  if (recipeEncontrado.length > 0) {
-    response.status(200).send(recipeEncontrado)
+  if (categoryEncontrado.length > 0) {
+    response.status(200).send(categoryEncontrado)
 
   } else {
-    response.status(404).send("Recipe não encontrado")
+    response.status(404).send("Category não encontrado")
   }
 
 };
 
-const saveRecipe = (request, response) => {
+const saveCategory = (request, response) => {
 
   const novoId = request.body.cpf + date()
   const novoUsuario = request.body
 
-  recipes.push(
+  categories.push(
     { id: novoId, ...novoUsuario }
   )
-  fs.writeFile("./scr/modal/dataRecipes.json", JSON.stringify(recipes), (err) => { console.log(err) })
-  response.status(201).send("Recipe incluido")
+  fs.writeFile("./scr/modal/dataCategorys.json", JSON.stringify(categories), (err) => { console.log(err) })
+  response.status(201).send("Category incluido")
 };
 
 
-const deleteRecipe = (request, response) => {
-  const recipeEncontrado = recipes.filter((recipe) => {
-    return recipe.id === Number(request.params.id)
+const deleteCategory = (request, response) => {
+  const categoryEncontrado = categories.filter((category) => {
+    return category.id === Number(request.params.id)
   })
-  if (recipeEncontrado.length > 0) {
-    posicao = recipeEncontrado.indexOf(recipeEncontrado.id);
-    recipes.splice(posicao, 1);
-    fs.writeFile("./scr/modal/dataRecipes.json", JSON.stringify(recipes), (err) => { console.log(err) })
+  if (categoryEncontrado.length > 0) {
+    posicao = categoryEncontrado.indexOf(categoryEncontrado.id);
+    categories.splice(posicao, 1);
+    fs.writeFile("./scr/modal/dataCategories.json", JSON.stringify(categories), (err) => { console.log(err) })
     response.status(200).send("Usuário Excluído")
 
   } else {
-    response.status(404).send("Recipe não encontrado")
+    response.status(404).send("Category não encontrado")
   }
 
 }
 
 
 
-const upDateRecipe = (request, response) => {
+const upDateCategory = (request, response) => {
 
-  const recipeEncontrado = recipes.filter((recipe) => {
-    return recipe.id === Number(request.params.id)
+  const categoryEncontrado = categories.filter((category) => {
+    return category.id === Number(request.params.id)
   })
-  if (recipeEncontrado.length > 0) {
-    posicao = recipeEncontrado.indexOf(recipeEncontrado.id);
-    const novoId = recipeEncontrado.id
-    recipes.splice(posicao, 1);
-    const novoRecipe = request.body
-    recipes.push(
-      { id: novoId, ...novoRecipe }
+  if (categoryEncontrado.length > 0) {
+    posicao = categoryEncontrado.indexOf(categoryEncontrado.id);
+    const novoId = categoryEncontrado.id
+    categories.splice(posicao, 1);
+    const novoCategory = request.body
+    categories.push(
+      { id: novoId, ...novoCategory }
     )
-    fs.writeFile("./scr/modal/dataRecipes.json", JSON.stringify(recipes), (err) => { console.log(err) })
-    response.status(201).send("Recipe incluido")
+    fs.writeFile("./scr/modal/dataCategories.json", JSON.stringify(categories), (err) => { console.log(err) })
+    response.status(201).send("Category incluido")
   };
 }
 
-module.exports = { allRecipes, searchRecipeForId, saveRecipe, deleteRecipe, upDateRecipe }
+module.exports = { allCategories, searchCategoryForId, saveCategory, deleteCategory, upDateCategory }
 
